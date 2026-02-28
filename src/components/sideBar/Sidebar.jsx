be250af8116ca90eff0416afb0ed97ccca1dce83
SafaBader <117042247+SafaBader@users.noreply.github.com>
2026-02-28 14:45:44 +0200

    add sidebar

---
+++ src/components/sideBar/Sidebar.jsx
@@ -0,0 +1,48 @@
+import { NavLink, useNavigate } from "react-router-dom";
+import { signOut } from "firebase/auth";
+import { auth } from "../../firebase/firebase";
+import "../sideBar/Sidebar.css";
+
+export default function Sidebar() {
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    try {
+      await signOut(auth);
+      navigate("/");
+    } catch (error) {
+      console.error("Logout error:", error);
+    }
+  };
+
+  return (
+    <aside className="sidebar">
+      <section className="sidebarBrand">
+        <span className="brandLogo" />
+          <p className="brandTitle">Smart Expense</p>
+      </section>
+
+      <nav className="nav">
+        <NavLink to="/home" end className={({ isActive }) => (isActive ? "navItem active" : "navItem")}>
+          Home
+        </NavLink>
+
+        <NavLink to="/home/goal" className={({ isActive }) => (isActive ? "navItem active" : "navItem")}>
+          Goal
+        </NavLink>
+
+        <NavLink to="/home/budgets" className={({ isActive }) => (isActive ? "navItem active" : "navItem")}>
+          Budgets
+        </NavLink>
+
+        <NavLink to="/home/transactions" className={({ isActive }) => (isActive ? "navItem active" : "navItem")}>
+          Transactions
+        </NavLink>
+      </nav>
+
+      <button className="logoutBtn" onClick={handleLogout}>
+        Logout
+      </button>
+    </aside>
+  );
+}
