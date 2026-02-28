00ed7885593099b58542df250e775acbf0fefccb
SafaBader <117042247+SafaBader@users.noreply.github.com>
2026-02-28 15:04:27 +0200

    add home layout

---
+++ src/layout/HomeLayout.jsx
@@ -0,0 +1,37 @@
+import { Outlet, useLocation } from "react-router-dom";
+import Sidebar from "../components/sideBar/Sidebar";
+import AppHeader from "../components/appHeader/AppHeader";
+import "../layout/HomeLayout.css";
+
+const headerMap = [
+  { match: /^\/home\/?$/, title: "Home", subtitle: "Your daily snapshot" },
+  { match: /^\/home\/goal\/?$/, title: "Savings Goal", subtitle: "Track your progress" },
+  { match: /^\/home\/budgets\/?$/, title: "Budgets", subtitle: "Plan and control spending" },
+  { match: /^\/home\/transactions\/?$/, title: "Transactions", subtitle: "Review your history" },
+];
+
+function getHeader(pathname) {
+  return headerMap.find((x) => x.match.test(pathname)) || {
+    title: "Smart Expense",
+    subtitle: "Stay on track",
+  };
+}
+
+export default function HomeLayout() {
+  const { pathname } = useLocation();
+  const { title, subtitle } = getHeader(pathname);
+
+  return (
+    <section className="homelayout">
+      <Sidebar />
+
+      <section className="appMain">
+        <AppHeader title={title} subtitle={subtitle}  />
+        
+        <main className="content">
+          <Outlet />
+        </main>
+      </section>
+    </section>
+  );
+}
