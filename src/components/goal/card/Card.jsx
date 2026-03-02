7320a1f8a250c370a11a4414bb8098812f039970
SafaBader <117042247+SafaBader@users.noreply.github.com>
2026-03-02 04:25:42 +0200

    update bath of card.css

--- src/components/goal/card/Card.jsx
+++ src/components/goal/card/Card.jsx
@@ -1,4 +1,4 @@
-import "./Card.css";
+import "../card/Card.css";
 
 export default function Card({ children, tone = "default", className = "" }) {
     return <section className={`card card--${tone} ${className}`}>{children}</section>;
